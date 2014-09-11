f663768ed0679156a4cb6870ab44b32bdf08fefc
Dario L Garcia <dario.garcia@kfgodel_dev>
2014-09-10 23:32:08 -0300

    Added collection specs

--- src/test/java/ar/com/kfgodel/tostring/CollectionRepresentationTest.java
+++ src/test/java/ar/com/kfgodel/tostring/CollectionRepresentationTest.java
@@ -6,6 +6,13 @@ import org.junit.runner.RunWith;
 import org.junit.runners.model.InitializationError;
 import org.junit.runners.model.RunnerBuilder;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * This type tests and defines behavior for collection representation
  * Created by kfgodel on 10/09/14.
@@ -15,23 +22,119 @@ public class CollectionRepresentationTest extends JavaSpec<StringerTestContext> 
     @Override
     public void define() {
 
-        describe("arrays, lists and sets", ()->{
-            it("are represented using enclosing brackets");
-            it("elements are separated with a comma and a space");
-            it("are truncated if their representation exceeds 60 chars");
-            it("are truncated if they have more than 5 elements and their representation exceeds 40 chars");
-            it("are not truncated if they have 5 or less elements and don't exceed 60 chars");
+        describe("arrays, lists and sets", ()-> {
+            beforeEach(()->{
+                context().object(ArrayList::new);
+            });
+            it("indicate size as first datum", () -> {
+                assertThat(Stringer.asString(context().object()))
+                        .startsWith("0#");
+            });
+            it("are represented using enclosing brackets", ()->{
+                assertThat(Stringer.asString(context().object()))
+                        .endsWith("[]");
+            });
+            it("elements are separated with a comma and a space", () -> {
+                List<Integer> list = (List<Integer>) context().object();
+                list.add(1);
+                list.add(2);
+                assertThat(Stringer.asString(list))
+                        .contains("1, 2");
+            });
+            it("are omitted if their representation exceeds 60 chars", ()->{
+                List<String> list = (List<String>) context().object();
+                list.add("Lorem ipsum dolor sit amet.");
+                list.add("Lorem ipsum dolor sit amet.");
+                list.add("Lorem ipsum dolor sit amet.");
+                list.add("Lorem ipsum dolor sit amet.");
+                assertThat(Stringer.asString(list))
+                        .contains("4#[\"Lorem ipsum dolor sit amet.\", \"Lorem ipsum dolor sit amet.\", \"Lorem ipsum dolor sit amet.\", ...]");
+            });
+            it("are omitted if they have more than 5 elements and their representation exceeds 40 chars", ()->{
+                List<Long> longList = createLongList();
+                assertThat(Stringer.asString(longList))
+                        .contains("100#[1, 2, 3, 4, 5, ...]");
+            });
+            it("are not omitted if they have 5 or less elements and don't exceed 60 chars", ()->{
+                List<String> list = (List<String>) context().object();
+                list.add("Lorem ipsum dolor sit amet.");
+                list.add("Lorem ipsum dolor sit amet.");
+                list.add("Lorem ipsum dolor sit amet.");
+                assertThat(Stringer.asString(list))
+                        .isEqualTo("3#[\"Lorem ipsum dolor sit amet.\", \"Lorem ipsum dolor sit amet.\", \"Lorem ipsum dolor sit amet.\"]");
+            });
         });
 
         describe("maps", ()->{
-            it("are represented with curly braces and key/value pairs");
-            it("key is separated from value with a colon and a space");
-            it("entries are separated with a comma and a space");
-            it("number keys are represented as numbers");
-            it("are truncated if their representation exceeds 60 chars");
-            it("are truncated if they have more than 5 elements and their representation exceeds 40 chars");
-            it("are not truncated if they have 5 or less elements and don't exceed 60 chars");
+            beforeEach(()->{
+                context().object(TreeMap::new);
+            });
+            it("indicate size as first datum", () -> {
+                assertThat(Stringer.asString(context().object()))
+                        .startsWith("0#");
+            });
+            it("are represented with curly braces and key/value pairs", ()->{
+                assertThat(Stringer.asString(context().object()))
+                        .endsWith("{}");
+            });
+            it("key is separated from value with a colon and a space", ()->{
+                Map<String, Integer> map = (Map<String, Integer>) context().object();
+                map.put("a", 1);
+                assertThat(Stringer.asString(context().object()))
+                        .contains("\"a\": 1");
+            });
+            it("entries are separated with a comma and a space", ()->{
+                Map<String, Integer> map = (Map<String, Integer>) context().object();
+                map.put("a", 1);
+                map.put("b", 2);
+                assertThat(Stringer.asString(context().object()))
+                        .contains("\"a\": 1, \"b\": 2");
+            });
+            it("number keys are represented as numbers", ()->{
+                Map<Double, Integer> map = (Map<Double, Integer>) context().object();
+                map.put(1.0, 1);
+                assertThat(Stringer.asString(context().object()))
+                        .contains("1.0: 1");
+            });
+            it("are omitted if their representation exceeds 60 chars", ()->{
+                Map<String, String> map = (Map<String, String>) context().object();
+                map.put("Lorem ipsum1", "Lorem ipsum dolor sit amet.");
+                map.put("Lorem ipsum2", "Lorem ipsum dolor sit amet.");
+                map.put("Lorem ipsum3", "Lorem ipsum dolor sit amet.");
+                map.put("Lorem ipsum4", "Lorem ipsum dolor sit amet.");
+                assertThat(Stringer.asString(context().object()))
+                        .isEqualTo("4#{\"Lorem ipsum1\": \"Lorem ipsum dolor sit amet.\", \"Lorem ipsum1\": \"Lorem ipsum dolor sit amet.\", ...}");
+            });
+            it("are omitted if they have more than 5 elements and their representation exceeds 40 chars", ()->{
+                Map<Double, Integer> longMap = createLongMap();
+                assertThat(Stringer.asString(longMap))
+                        .isEqualTo("100#{1.0: 1, 2.0: 2, 3.0: 3, 4.0: 4, 5.0: 5, 6.0: 6, 7.0: 7 ,...}");
+
+            });
+            it("are not omitted if they have 5 or less elements and don't exceed 60 chars", ()->{
+                Map<String, String> map = (Map<String, String>) context().object();
+                map.put("Lorem ipsum1", "Lorem ipsum dolor sit amet.");
+                map.put("Lorem ipsum2", "Lorem ipsum dolor sit amet.");
+                assertThat(Stringer.asString(context().object()))
+                        .isEqualTo("2#{\"Lorem ipsum1\": \"Lorem ipsum dolor sit amet.\", \"Lorem ipsum1\": \"Lorem ipsum dolor sit amet.\"}");
+            });
         });
 
+    }
+
+    private List<Long> createLongList() {
+        List<Long> longList = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            longList.add((long) i);
+        }
+        return longList;
+    }
+
+    private Map<Double, Integer> createLongMap() {
+        Map<Double, Integer> longMap = new TreeMap<>();
+        for (int i = 0; i < 100; i++) {
+            longMap.put((double) i, i);
+        }
+        return longMap;
     }
 }
